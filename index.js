890235465ad20d4ba3d328b46864734b45b7ea48
delta911ee <hassan911ee@gmail.com>
2024-02-24 01:33:07 +0400

    package

---
+++ index.js
@@ -0,0 +1,26 @@
+/*
+A utility function to climb the folder/file/diirectory heirarchy.
+path: String, the path to be modified.
+level: int, levels to be climbed.
+*/
+
+function climbDir(path, level){
+    let items = path.split("\\");
+    let result = '';
+
+    if(items.length < level){
+        return;
+    }
+    
+    for(let index = 0; index<level; index++){
+        items.pop();
+    }
+    
+    items.forEach(item=>{
+        result += item+"\\";
+    })
+
+    return result;
+}
+
+module.exports = climbDir;
